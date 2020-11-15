811f8c77eb743f1654c14c0434f60d961d032eaa
Ágota Ékes <ekesagi@gmail.com>
2020-11-15 20:45:37 +0100

    commit

--- week08/WindowsFormsApp1/Form1.cs
+++ week08/WindowsFormsApp1/Form1.cs
@@ -51,5 +51,37 @@ namespace WindowsFormsApp1
                 _balls.Remove(oldestBall);
             }
         }
+
+        private Toy _nextToy;
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+        }
     }
 }
