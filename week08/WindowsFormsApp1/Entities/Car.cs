7ed3328af39e24df123b1b137a0f22b77973a393
Ágota Ékes <ekesagi@gmail.com>
2020-11-15 19:22:43 +0100

    commit

---
+++ week08/WindowsFormsApp1/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
