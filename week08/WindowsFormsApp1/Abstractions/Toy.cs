dc62214f2812130839988351b553653c2ba724d3
Ágota Ékes <ekesagi@gmail.com>
2020-11-15 19:16:28 +0100

    commit

--- week08/WindowsFormsApp1/Abstractions/Toy.cs
+++ week08/WindowsFormsApp1/Abstractions/Toy.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1.Abstractions
 {
-    class Toy
+    public abstract class Toy : Label
     {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
     }
 }
